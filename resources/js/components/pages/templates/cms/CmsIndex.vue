353e30953e2732f3f93d59889a31a297059ffea0
Alvin Katapusan <alvinkatapusan@gmail.com>
2020-07-16 00:25:33 +0800

    Setup CMS template  ~admin LTE 3

--- resources/js/components/pages/templates/cms/CmsIndex.vue
+++ resources/js/components/pages/templates/cms/CmsIndex.vue
@@ -1,13 +1,46 @@
+
 <template>
   <div>
-      <router-view></router-view>
+      
+      <div class="wrapper">
+        <!-- Navbar -->
+          <Navbar/>
+        <!-- ./ Navbar -->
+
+        <!-- Main Sidebar Container -->
+            <MainSideBar/>
+        <!--  ./Main Sidebar Container -->
+
+        <!-- Content Wrapper. Contains page content -->
+          <ContentWrapper/>
+        <!-- /.content-wrapper -->
+
+        <!-- Control Sidebar -->
+           <ControlSideBar/>
+        <!-- /.control-sidebar -->
+
+        <!-- Main Footer -->
+           <Footer/>
+         <!-- /.Mai Footer -->
+      </div>
   </div>
 </template>
 
 <script>
 
+import Navbar from "./navbar"
+import MainSideBar from './SideBar'
+import  ContentWrapper from './ContentWrapper'
+import ControlSideBar from './controlSideBar'
+import Footer from './Footer'
 export default {
- 
+    components : {
+          Navbar,
+          MainSideBar,
+          ContentWrapper,
+          ControlSideBar,
+          Footer
+    }
 }
 </script>
 
---
+++ resources/js/components/pages/templates/cms/controlSideBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+         <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+            <div class="p-3">
+            <h5>Title</h5>
+            <p>Sidebar content</p>
+         </div>
+  </aside>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
